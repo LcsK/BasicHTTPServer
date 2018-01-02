cf738b63ceb3587698116fb6dada554bf6714311
Lucas Lopes <lucas.ferreira@ibm.com>
2018-01-02 10:31:49 -0200

    Implementa um cliente HTTP simples

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const connect = require('connect');
+const request = require('request');
 
-var app = connect()
-	.use(function(req,res) {
-		if(req.url == "/hello") {
-			console.log("sending plain");
-			res.end("Hello from app");
-		}
-		else if(req.url == "/hello.json") {
-			console.log("sending json");
-
-			var data = "Hello";
-			var jsonData = JSON.stringify(data);
-
-			res.setHeader('Content-Type','application/json');
-			res.end(jsonData);
-		}
-		else if(req.url == "/statusCodeDemo") {
-			console.log("sending 404 status code");
-			res.statusCode = 404;
-			res.end("Oops, could not find something");
-		}
-	})
-	.listen(80);
-
-console.log("Listening on port 80");
+request('http://www.google.com', function(error, response, body) {
+	if(!error && response.statusCode == 200) {
+		console.log(body);
+	}
+	if(error) {
+		console.log(error);
+	}
+});
