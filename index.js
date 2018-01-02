b38db92ef7ce22fb58b5645403df8cdf8d1ae308
Lucas Lopes <lucas.ferreira@ibm.com>
2018-01-02 13:03:51 -0200

    Implementa servidor gerenciador de Eventos simples

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-const express = require('express');
-const formidable = require('formidable');
-const app = express();
+console.log("Handling Web Server Events");
 
-app.use(function(req, res) {
-	if(req.method.toLowerCase() == "post") {
-		//configure the form
-		form.uploadDir = __dirname + '/uploads';
-		form.keepExtensions = true;
-		form.type = "multipart";
+const http = require('http');
 
-		form.parse(req, function(err,fields,files) {
-			//Parse fields
-			var firstName = fields.userFirstName;
-			var lastName = fields.userLastName;
-			console.log("User info parsed from form: " + firstName + " " + lastName);
-			res.writeHead(200, {'content-type': 'text/plain'});
-			res.end("Form data recieved");
-		})
-	}
+const server = http.createServer();
+
+server.on('request',function(req,res) {
+	console.log("Request received", req.headers);
+	res.end("Thanks, I got your request");
 });
 
-//Start server
+server.on('upgrade',function(req,socket,head){
+	console.log("Upgrade the connection to a web socket connection");
+});
+
 const port = 80;
-app.listen(port);
-console.log("Listening on port 80");
+server.listen(port,function() {
+	console.log("HTTP Server Listening");
+});
